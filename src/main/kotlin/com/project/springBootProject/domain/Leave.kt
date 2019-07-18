01b7fcdd8d3ef0880fd2b4ccacb39f6ccec5c282
Watchara <watchara@ampostech.com>
2019-07-18 11:35:10 +0700

    fix mysql reserved word

--- src/main/kotlin/com/project/springBootProject/domain/Leave.kt
+++ src/main/kotlin/com/project/springBootProject/domain/Leave.kt
@@ -6,9 +6,11 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
-class Leave {
+@Table(name="leaveDetail")
+class LeaveDetail {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     var id: Int? = null
